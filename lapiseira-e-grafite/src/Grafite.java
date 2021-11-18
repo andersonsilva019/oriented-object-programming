4939fa0f8ed87cc136707d3e5850c02faaa20f19
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-11-18 18:39:36 -0300

    lapiseira v1

---
+++ lapiseira-e-grafite/src/Grafite.java
@@ -0,0 +1,56 @@
+class Grafite {
+  private float calibre;
+  private String dureza;
+  private int tamanho;
+
+  Grafite(float calibre, String dureza, int tamanho) {
+    this.calibre = calibre;
+    this.dureza = dureza;
+    this.tamanho = tamanho;
+  }
+
+  public String getDureza() {
+    return this.dureza;
+  }
+
+  public float getCalibre() {
+    return this.calibre;
+  }
+
+  public void setTamanho(int tamanho) {
+    this.tamanho = tamanho;
+  }
+
+  public int getTamanho() {
+    return this.tamanho;
+  }
+
+  public String toString() {
+    return "[" + this.calibre + ":" + this.dureza + ":" + this.tamanho + "]";
+  }
+
+  public int desgastePorFolha() {
+
+    int desgaste = 0;
+
+    switch (this.dureza) {
+    case "HB":
+      desgaste = 1;
+      break;
+    case "2B":
+      desgaste = 2;
+      break;
+    case "4B":
+      desgaste = 4;
+      break;
+    case "6B":
+      desgaste = 6;
+      break;
+    default:
+      desgaste = -1;
+      break;
+    }
+
+    return desgaste;
+  }
+}
---
+++ lapiseira-e-grafite/src/Lapiseira.java
@@ -0,0 +1,84 @@
+class Lapiseira {
+
+  /**
+   * Declaração dos atributos
+   */
+  private float calibre;
+  private Grafite grafite;
+
+  // Criando uma constante com o tamanho mínimo do grafite
+  private static final int TAMANHO_MIN_GRAFITE = 10;
+
+  /**
+   * Método construtor
+   * 
+   * @param calibre calibre inicial da lapiseira
+   */
+  public Lapiseira(float calibre) {
+    this.calibre = calibre;
+    this.grafite = null;
+  }
+
+  /**
+   * @return retorna uma string formatada [calibre: *, grafite: *]
+   */
+  public String toString() {
+    return "calibre: " + this.calibre + ", grafite: " + this.grafite;
+  }
+
+  /**
+   * Método que realiza uma inserção do grafite na lapiseira
+   * 
+   * @param grafite
+   * @return retorna true caso a inserção tenha sido feita. Caso contrário,
+   *         retorna false
+   */
+  public boolean inserir(Grafite grafite) {
+    if (this.calibre != grafite.getCalibre()) {
+      System.out.println("fail: calibre incompatível");
+      return false;
+    } else if (this.grafite != null) {
+      System.out.println("fail: ja existe grafite");
+      return false;
+    } else {
+      this.grafite = grafite;
+      return true;
+    }
+  }
+
+  /**
+   * Método que remove um grafite da lapiseira
+   * 
+   * @return retorna o grafite removido
+   */
+  public Grafite remover() {
+    if (this.grafite == null) {
+      System.out.println("fail: nao existe grafite");
+      return null;
+    } else {
+      Grafite grafiteRef = this.grafite;
+      Grafite grafiteTemp = new Grafite(grafiteRef.getCalibre(), grafiteRef.getDureza(), grafiteRef.getTamanho());
+      this.grafite = null;
+      return grafiteTemp;
+    }
+  }
+
+  public void escrever() {
+    Grafite grafiteRef = this.grafite;
+    int desgaste = grafiteRef.desgastePorFolha();
+
+    if (grafiteRef.getTamanho() <= TAMANHO_MIN_GRAFITE) {
+      grafiteRef = null;
+      System.out.println("warning: grafite acabou");
+    } else if (grafiteRef.getTamanho() - desgaste == TAMANHO_MIN_GRAFITE) {
+      grafiteRef.setTamanho(grafiteRef.getTamanho() - desgaste);
+      System.out.println("warning: grafite acabou");
+    } else if (grafiteRef.getTamanho() - desgaste < TAMANHO_MIN_GRAFITE) {
+      System.out.println("fail: folha incompleta");
+      System.out.println("warning: grafite acabou");
+      grafiteRef.setTamanho(TAMANHO_MIN_GRAFITE);
+    } else {
+      grafiteRef.setTamanho(grafiteRef.getTamanho() - desgaste);
+    }
+  }
+}

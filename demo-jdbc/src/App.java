c139cfc9d5711678731d84a6bc7405694da7313b
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-12-25 21:13:23 -0300

    feat: create Seller entity class

--- demo-jdbc/src/App.java
+++ demo-jdbc/src/App.java
@@ -1,8 +1,13 @@
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department dep = new Department(1, "D1");
-        System.out.println(dep.toString());
+        System.out.println(dep);
+        Seller seller = new Seller(1, "Seller1", "seller@seller.com", new Date(), 1000.0, dep);
+        System.out.println(seller);
     }
 }
